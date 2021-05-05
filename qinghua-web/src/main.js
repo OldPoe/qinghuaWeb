2a1a3fb87e9ca93de40c53bde7cfcec84bfcc5fd
OldPoe <zhoux96@outlook.com>
2021-05-05 15:39:33 +0800

    elementPlus

--- qinghua-web/src/main.js
+++ qinghua-web/src/main.js
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import './plugins/element.js'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import './plugins/element.js'
+// import ElementUI from 'element-ui'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 import router from './router'
 import store from './store'
 
-App.use(Element)
-createApp(App).use(store).use(router).mount('#app')
+// App.use(ElementUI)
+// createApp(App).use(store).use(router)
+//     .use(ElementPlus)
+//     .mount('#app')
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.use(store)
+app.use(router)
+app.mount('#app')
