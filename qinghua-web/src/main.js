0dae3d9bd68f8613383619d645a526511f1fb66b
OldPoe <zhoux96@outlook.com>
2021-05-05 14:58:46 +0800

    安装element,vue-router,vuex

--- qinghua-web/src/main.js
+++ qinghua-web/src/main.js
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './plugins/element.js'
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+import store from './store'
 
-createApp(App).mount('#app')
+App.use(Element)
+createApp(App).use(store).use(router).mount('#app')
